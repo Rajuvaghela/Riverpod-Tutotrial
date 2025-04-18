64c1be01d1ee0d6a1b2b3f81b205a067ab40b2b9
Raju <rajuvaghela1485@gmail.com>
2025-04-18 19:44:22 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_tutorial/post_screen.dart';
+import 'package:riverpod_tutorial/slider_screen.dart';
+import 'package:riverpod_tutorial/stream_provider_example.dart';
+
+import 'favourite_screen.dart';
+import 'future_provider.dart';
+import 'login_screen.dart';
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ProviderScope(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+
+        home:  LoginScreen(),
+      ),
+    );
+  }
+}
+
